d99fda37fd1f360c646f7496c1834131870c5542
bunty989 <alok.mozart@gmail.com>
2023-03-30 00:35:51 +1100

    Initial Commit

---
+++ NSWEHealth/Amazon/Setup/SetAppUrl.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using NSWEHealth.Framework.Wrapper;
+using ConfigType = NSWEHealth.Framework.Wrapper.TestConstant.ConfigTypes;
+using ConfigKey = NSWEHealth.Framework.Wrapper.TestConstant.ConfigTypesKey;
+
+namespace NSWEHealth.Amazon.Setup
+{
+    internal class SetAppUrl
+    {
+        public static string SetUrl(string protocol)
+        {
+            var url = ConfigHelper.ReadConfigValue(ConfigType.AppConfig, ConfigKey.AppUrl);
+            protocol = protocol.ToLower() switch
+            {
+                "http" => "http://",
+                "https" => "https://",
+                _ => protocol
+            };
+            Log.Information("The Application URl for the application is set as {0}", protocol + url);
+            return protocol + url;
+        }
+    }
+}
---
+++ NSWEHealth/Framework/Wrapper/ConfigHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace NSWEHealth.Framework.Wrapper
+{
+    internal class ConfigHelper
+    {
+        public static string ReadConfigValue(string configType, string keyValue)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(TestConstant.PathVariables.ConfigFileName).Build();
+            return config.GetValue<string>(configType + keyValue);
+        }
+    }
+}
